cad4ecc35fe6d7ff951908e2801c9dcd1a3ec835
ddziechciarz <alawon12@gmail.com>
2023-10-14 17:40:52 +0200

    added constants file

--- Assets/Utility/FloatingText/floating_text_spawner.gd
+++ Assets/Utility/FloatingText/floating_text_spawner.gd
@@ -8,7 +8,6 @@ enum text_types {
 
 static var floating_text = preload("res://Assets/Utility/FloatingText/floating_text.tscn")
 
-
 static func create_floating_text(parent: Node, text, type: text_types) -> void:
 	var text_instance : FloatingText = floating_text.instantiate()
 	text_instance.amount = text
@@ -16,7 +15,12 @@ static func create_floating_text(parent: Node, text, type: text_types) -> void:
 	var color = Color(1,1,1,1)
 	match type:
 		text_types.DAMAGE:
-			color = Color(1,0,0,1)
+			color = ColorConstants.damage_color
+		text_types.CRITICAL:
+			text_instance.scale = Vector2(1.35, 1.35)
+			color = ColorConstants.crit_color
+		text_types.HEALING:
+			color = ColorConstants.healing_color
 	
 	text_instance.font_color = color 
 	parent.add_child(text_instance)
