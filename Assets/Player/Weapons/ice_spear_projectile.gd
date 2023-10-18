f4fd21bbb8d6b940714c231d14362196e3b422b5
Mlodkoo <63650085+Mlodko@users.noreply.github.com>
2023-10-18 17:49:57 +0200

    Ice Spear working
    - [x] Spawning projectiles + framework
    - [x] Detecting/choosing enemies
    - [x] Dealing damage
    - [ ] Tornado

--- Assets/Player/Weapons/ice_spear_projectile.gd
+++ Assets/Player/Weapons/ice_spear_projectile.gd
@@ -7,14 +7,14 @@ var attack_size = 1.0
 @onready var collisionShape = $CollisionShape2D
 
 
-var target = Vector2.ZERO
+var target = Vector2.ZERO # Global position of target
 var angle = Vector2.ZERO
 #@onready var player = get_tree().get_first_node_in_group("Player")
 
 func _ready():
 	$DestroyTimer.start()
 	angle = global_position.direction_to(target)
-	rotation = angle.angle() + deg_to_rad(135)
+	rotation = angle.angle()
 	set_speed(100) # Default
 	
 	
@@ -31,3 +31,8 @@ func set_damage(val : float): # Override from Projectile
 	
 func set_attack_speed(val : float):
 	$DamageBox.attack_speed = val
+
+
+func _on_damage_box_area_entered(area):
+	hp -= 1
+	if hp <= 0: queue_free()
--- Assets/Utility/Weapons/projectile_spawner.gd
+++ Assets/Utility/Weapons/projectile_spawner.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var damage = 2
 @export var speed = 100
+@export var projectile_hp = 1
 
 func get_targets(): # Returns array of Area2Ds on the 3rd collision layer or null(!) if none are present
 	# PS I hate nulls, if anyone knows of a better way to do this please tell me
@@ -13,21 +14,40 @@ func get_targets(): # Returns array of Area2Ds on the 3rd collision layer or nul
 func set_detection_radius(r : float): # r is radius of circle in [px]
 	$DetectionCircle/CollisionShape2D.shape.radius = r
 	
-func spawn_projectile(projectileScene: Projectile):
+func get_detection_radius():
+	return $DetectionCircle/CollisionShape2D.shape.radius
+	
+func spawn_projectile(projectileScene):
 	# projectile has to inherit the class, it ensures that there are setters for damage and speed
 	# TODO should probably put collisionShape in there too
 	var targets = get_targets()
 	if targets == null: return
 	
-	var target = targets[randi() % len(targets)] # Select random target
+	# Select random targets
+	var target : Area2D = distance_weighted_random_choice(targets, get_detection_radius()) 
 	
 	# Prepare projectile for spawn
 	var projectile = projectileScene.instantiate()
 	projectile.set_damage(damage)
 	projectile.set_speed(speed)
-	projectile.collisionShape.transform.look_at(target.transform.position)
+	projectile.hp = projectile_hp
+	projectile.global_position = global_position
+	projectile.target = target.global_position
+
 	
 	#Spawn
 	$ProjectileHolder.add_child(projectile)
+	
+func distance_weighted_random_choice(array : Array[Area2D], max_distance : float):
+	# Basically the closer the target is the higher the chance it will get targeted
+	if len(array) == 1:
+		return array[0]
+	var i : int = 0
+	while true:
+		var target : Area2D = array[i % len(array)]
+		if randf() >= 1 - global_position.distance_to(target.global_position) / max_distance:
+			return target
+		i += 1
+
 
 
