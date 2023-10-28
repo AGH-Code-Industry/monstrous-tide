ccffa3fd6b9d494730ccf5ba136995f28e882338
Bkasprzyk2077 <dolarbartek@wp.pl>
2023-10-28 22:11:43 +0200

    ItemDropComponent update & UI update
    items now drops from an array

--- Assets/Utility/Managers/experience_manager.gd
+++ Assets/Utility/Managers/experience_manager.gd
@@ -2,12 +2,14 @@ extends Node
 
 @export var points_to_levelup: int = 10
 var current_points = 0
-var current_level = 0
+var current_level = 1
 
 
 func _ready():
 	GameEvents.experience_collected.connect(on_experience_collected)
-	GameEvents.emit_update_experience_bar(current_points, points_to_levelup)
+	GameEvents.emit_update_experience_bar(current_points, points_to_levelup, current_level)
+	
+	
 func level_up():
 	print("New level!")
 	current_level += 1
@@ -19,4 +21,4 @@ func on_experience_collected(number_of_experience: int):
 	current_points += number_of_experience
 	if current_points >= points_to_levelup:
 		level_up()
-	GameEvents.emit_update_experience_bar(current_points, points_to_levelup)
+	GameEvents.emit_update_experience_bar(current_points, points_to_levelup, current_level)
--- Assets/Utility/UI/Progressbar/progress_bar.gd
+++ Assets/Utility/UI/Progressbar/progress_bar.gd
@@ -1,12 +1,15 @@
 extends CanvasLayer
 
-@onready var progress_bar = $MarginContainer/ProgressBar
+@onready var progress_bar = $MarginContainer/VBoxContainer/ProgressBar
+@onready var level_info = $MarginContainer/VBoxContainer/LevelInfo
 
 
 func _ready():
 	GameEvents.update_experience_bar.connect(on_update_experience_bar)
 	
 
-func on_update_experience_bar(current_points: int, points_to_level_up: int):
+func on_update_experience_bar(current_points: int, points_to_level_up: int, current_level: int):
+	
 	progress_bar.value = current_points
 	progress_bar.max_value = points_to_level_up
+	level_info.text = "Level: " + str(current_level)
