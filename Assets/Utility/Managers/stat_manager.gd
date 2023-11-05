89b5dc7ae4aff2d7c51ca0ca6763d0955f840995
ddziechciarz <alawon12@gmail.com>
2023-11-05 18:08:24 +0100

    added comments

--- Assets/Utility/Managers/stat_manager.gd
+++ Assets/Utility/Managers/stat_manager.gd
@@ -6,7 +6,7 @@ func emit_player_stats_update(stats: Dictionary, time: float = -1) -> void:
 	emit_stats_with_delay(update_player_stats, stats, time)
 
 
-
+# abstract way of emitting proper signal with specified stats
 func emit_stats_with_delay(target_signal: Signal, stats: Dictionary, time: float):
 	target_signal.emit(stats)
 
@@ -14,7 +14,8 @@ func emit_stats_with_delay(target_signal: Signal, stats: Dictionary, time: float
 		await get_tree().create_timer(time).timeout
 		target_signal.emit(negate_stats(stats))
 		
-
+# needed for adding negative stats to create short term buffs:
+# first add stats, after some delay subtract them
 func negate_stats(stats: Dictionary) -> Dictionary:
 	var negated_stats = {}
 	
@@ -23,6 +24,10 @@ func negate_stats(stats: Dictionary) -> Dictionary:
 	
 	return negated_stats
 	
+
+# adds only stats that the object is interested in
+# eg. hitbox will receive dodge and hp updates but won't register
+# pickup radius increase
 static func add_relevant_stats(currentStats: Dictionary, incomingStats: Dictionary) -> Dictionary:
 	var updatedStats = currentStats.duplicate()
 
