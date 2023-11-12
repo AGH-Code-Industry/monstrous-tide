50f73e277622a2a20cd234eecceacbed46da887f
ddziechciarz <alawon12@gmail.com>
2023-11-12 17:31:47 +0100

    print() cleanup

--- Assets/Player/Weapons/AoEWeapons/Lantern/lantern.gd
+++ Assets/Player/Weapons/AoEWeapons/Lantern/lantern.gd
@@ -5,4 +5,3 @@ class_name Lantern extends AoEWeapon
 func _ready() -> void:
 	super()
 	possible_upgrades[0].upgrades[0].apply_upgrade()
-	print(stat_set.get_stat_value(Stat.Type.DAMAGE))
--- Assets/Player/Weapons/WeaponUpgrades/weapon_stat_upgrade.gd
+++ Assets/Player/Weapons/WeaponUpgrades/weapon_stat_upgrade.gd
@@ -3,6 +3,4 @@ class_name WeaponStatUpgrade extends WeaponUpgrade
 @export var stats : Array[Stat]
 
 func apply_upgrade():
-	print("tried adding upgrade")
-	print(weapon_ref)
 	weapon_ref.add_stats(stats)
