e92dc2afc2a10c4c50f12df58d09cfff57372a3a
ddziechciarz <alawon12@gmail.com>
2023-11-12 17:23:58 +0100

    Added weapon manager

--- Assets/Player/Weapons/WeaponUpgrades/upgrade_tier.gd
+++ Assets/Player/Weapons/WeaponUpgrades/upgrade_tier.gd
@@ -1,4 +1,3 @@
 class_name UpgradeTier extends Resource
 
 @export var upgrades : Array[WeaponUpgrade] = []
-
---
+++ Assets/Utility/Managers/weapon_manager.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var weapons : Array[Weapon] = []
+
+# Used fro choosing random weapon as a reward
+var all_possible_weapons : Array[Weapon] = []
+
+func _ready() -> void:
+	# Get weapons player started with
+	for node in $"../Weapons".get_children():
+		if typeof(node) == typeof(Weapon):
+			weapons.append(node)
+
+
