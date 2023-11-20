12771a95fbf4b4cb61bb8bdd7cab438801fa1efc
Sara Libirt <sara.149081@gmail.com>
2023-11-20 23:49:15 +0100

    Add Animation to Script

--- Assets/Player/player.gd
+++ Assets/Player/player.gd
@@ -5,11 +5,14 @@ var movement_speed: float = 40.0
 var last_movement := Vector2.UP
 @export var DEBUG_heal_value : float = 1
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+
 @export var stats = {StatConstants.PlayerStats.MOVEMENTSPEED: 5}
 
 func _ready():
 	animation_sprite = get_node("AnimatedSprite2D")
 	animation_sprite.play("Idle")
+	animation_tree.active = true
 	StatManager.update_player_stats.connect(func(receivedStats): stats = StatManager.add_relevant_stats(stats, receivedStats))
 
 func _physics_process(_delta):
@@ -29,11 +32,15 @@ func movement():
 	if mov != Vector2.ZERO:
 		last_movement = mov
 		animation_sprite.play("Walk")
+		animation_tree["parameters/conditions/idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
 		if mov[0] > 0:
 			animation_sprite.flip_h = true
 		elif mov[0] < 0:
 			animation_sprite.flip_h = false
 	else:
+		animation_tree["parameters/conditions/idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
 		animation_sprite.play("Idle")
 	velocity = mov.normalized() * stats[StatConstants.PlayerStats.MOVEMENTSPEED]
 	move_and_slide()
@@ -45,3 +52,5 @@ func die():
 
 func _on_hit_box_on_death() -> void:
 	die()
+	
+
