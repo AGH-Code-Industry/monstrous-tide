44007f5c4855f4f02a7295231ee7d72e884f20ae
Bkasprzyk2077 <dolarbartek@wp.pl>
2023-11-26 12:45:40 +0100

    Multicard bug fixes

--- Assets/Utility/UI/LvlUps/CardPopupMenu.gd
+++ Assets/Utility/UI/LvlUps/CardPopupMenu.gd
@@ -9,13 +9,23 @@ func _ready():
 
 
 func create_cards():
-	get_tree().paused = true
 	for i in range(3):
 		var level_up_card_instance = level_up_card.instantiate()
 		h_box_container.add_child(level_up_card_instance)
+		
+		
+func remove_cards():
+	for card in h_box_container.get_children():
+		card.queue_free()
 
 
 func on_level_up_signal(levels_to_lvlup: int):
+	if len(h_box_container.get_children()) > 0:
+		await GameEvents.upgrade_selected
+		print("ready")
 	for i in levels_to_lvlup:
+		get_tree().paused = true
 		create_cards()
 		await GameEvents.upgrade_selected
+		remove_cards()
+		get_tree().paused = false
--- Assets/Utility/UI/LvlUps/LvlUpCard.gd
+++ Assets/Utility/UI/LvlUps/LvlUpCard.gd
@@ -13,22 +13,26 @@ func _ready():
 	await animation_player.animation_finished
 	disabled = false
 
+
+func play_out_animation():
+	for card in get_parent().get_children():
+		card.animation_player.play("out")
+
+
+func disable_cards():
+	for card in get_parent().get_children():
+		card.disabled = true
+
 	
 func select_card():
-	disabled = true
-	for card in get_parent().get_children():
-		if card == self:
-			continue
-		card.disabled = true
-		card.animation_player.play("out")
-	get_tree().paused = false
-	animation_player.play("out")
+	disable_cards()
+	play_out_animation()
 	await animation_player.animation_finished
 	GameEvents.emit_upgrade_selected()
 	
 
 func on_input_gui(event: InputEvent):
-	modulate = Color.WHITE
+#	modulate = Color.WHITE
 	if disabled:
 		return
 	if event.is_action_pressed("left_click"):
@@ -36,14 +40,14 @@ func on_input_gui(event: InputEvent):
 	
 	
 func on_mouse_entered():
-	modulate = Color.WHITE
+#	modulate = Color.WHITE
 	if disabled:
 		return
 	animation_player.play("hover")
 
 
 func on_mouse_exited():
-	modulate = Color.WHITE
+#	modulate = Color.WHITE
 	if disabled:
 		return
 	animation_player.play_backwards("hover")
