46fc78c3ef6cd32b225eb3a125f38ff4b8332cb5
pkepowicz <paw.kepowicz@gmail.com>
2023-12-04 20:54:57 +0100

    initial stat config

--- Assets/Player/Weapons/AoEWeapons/Lantern/lantern.gd
+++ Assets/Player/Weapons/AoEWeapons/Lantern/lantern.gd
@@ -2,6 +2,13 @@ class_name Lantern extends AoEWeapon
 
 @export var starting_stats: Array[Stat]
 
+# components
+@onready var DamageBox = $DamageBox
+
 func _ready() -> void:
 	super()
 	possible_upgrades[0].upgrades[0].apply_upgrade()
+	self.scale = Vector2(stat_set.get_stat_value(Stat.Type.PICKUPRADIUS), stat_set.get_stat_value(Stat.Type.PICKUPRADIUS))
+	DamageBox.damage = stat_set.get_stat_value(Stat.Type.DAMAGE)
+	DamageBox.attack_speed = stat_set.get_stat_value(Stat.Type.ATTACKSPEED)
+
