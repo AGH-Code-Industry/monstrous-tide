3428b4b3158a7bb729732da73c13712ab39322b9
Bkasprzyk2077 <dolarbartek@wp.pl>
2024-02-19 17:15:46 +0100

    resume & exit code

--- Assets/MenuInterfaces/PauseMenu.gd
+++ Assets/MenuInterfaces/PauseMenu.gd
@@ -8,6 +8,20 @@ func _ready():
 
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
-		get_tree().paused = false
-		queue_free()
-		get_tree().root.set_input_as_handled()
+		_on_resume_pressed()
+
+
+func _on_resume_pressed():
+	get_tree().paused = false
+	queue_free()
+	get_tree().root.set_input_as_handled()
+
+
+func _on_options_button_pressed():
+	print("TODO in future")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+
