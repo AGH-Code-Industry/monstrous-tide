8c23779da4cbd3f8c8c62c40cac76dae5bd0324b
Bkasprzyk2077 <dolarbartek@wp.pl>
2024-02-20 12:07:17 +0100

    create_special_gem function

--- Assets/Utility/DropItems/ItemDropComponent.gd
+++ Assets/Utility/DropItems/ItemDropComponent.gd
@@ -3,6 +3,10 @@ extends Node
 @export var hit_box_component: Node
 @export var items_to_drop: Array[DropItem] = []
 
+static var current_items_count = 0
+static var item_count_limit = 1
+static var last_item
+
 func _ready():
 	if hit_box_component == null:
 		return
@@ -10,7 +14,10 @@ func _ready():
 
 
 func on_death_drop_crystal():
-	pick_random_item()
+	if current_items_count < item_count_limit:
+		create_item(pick_random_item())
+	else:
+		create_special_gem(pick_random_item())
 	
 
 func pick_random_item():
@@ -25,14 +32,23 @@ func pick_random_item():
 	var offset: float = 0
 	for item in items_to_drop:
 		if random_number < item.drop_chance + offset:
-			create_item(item.item_scene)
-			return
+			return item.item_scene
 		else:
 			offset += item.drop_chance
 			
 func create_item(item: PackedScene):
+	current_items_count += 1
 	var owner_position = owner.global_position
 	var item_instance = item.instantiate()
 	var items_layer = get_tree().get_first_node_in_group("items")
 	items_layer.call_deferred("add_child", item_instance)
 	item_instance.global_position = owner_position
+	
+	
+func create_special_gem(item: PackedScene):
+	var item_instance = item.instantiate()
+	if "experience_points" in item_instance:
+		print(item_instance.experience_points)
+	else:
+		print("NO POINTS")
+	item_instance.queue_free()
