03acb6c2c988aaa9f19a0633ccdc3afd5f327e47
Mlodko <mlodkowskip@gmail.com>
2024-02-26 11:09:15 +0100

    First changes
    - Added in-code documentation
    - Added map/filter functionality to instantiating projectiles and searching for targets
    - Added first features for the Projectile class

--- Assets/Utility/Weapons/projectile.gd
+++ Assets/Utility/Weapons/projectile.gd
@@ -1,11 +1,19 @@
+## This resource holds all information required to instantiate a projectile.
 class_name Projectile
-extends Area2D
+extends Resource
 
-@export var damage : int
+## Damage per projectile
+@export var damage : float
+
+## The amount of times a projectile fires per second (1s / {attack_speed})
 @export var attack_speed : int
 
-func set_damage(val):
-	damage = val
-	
-func set_attack_speed(val):
-	attack_speed = val
+## Projectile's speed
+@export var speed : int
+
+## The amount of projectiles spawned per attack
+@export var projectiles_per_attack : int
+
+## Projectile's scene
+@export var scene : PackedScene
+
--- Assets/Utility/Weapons/projectile_spawner.gd
+++ Assets/Utility/Weapons/projectile_spawner.gd
@@ -1,27 +1,70 @@
+## This class handles the targeting and spawning of projectiles.
+## It's [Node] should be a child of the weapon [Node]
 extends Node2D
 
-func get_targets(): # Returns array of Area2Ds on the 3rd collision layer or null(!) if none are present
-	# PS I hate nulls, if anyone knows of a better way to do this please tell me
-	# - Mlodko
-	if not $DetectionCircle.has_overlapping_areas():
-		return null
-	return $DetectionCircle.get_overlapping_areas()
-	
-func set_detection_radius(r : float):
-	$DetectionCircle/CollisionShape2D.shape.radius = r
-	
-func get_detection_radius():
-	return $DetectionCircle/CollisionShape2D.shape.radius
-	
-func spawn_projectile(projectileScene : PackedScene) -> Node:
-	# Prepare projectile for spawn
-	var projectile = projectileScene.instantiate()
+func _default_map_func(projectile : Node) -> Node:
 	projectile.global_position = global_position
-	
-	# Return
 	return projectile
 
-func distance_weighted_random_choice(array : Array[Area2D], max_distance : float):
+func _default_filter_func(_x):
+	return true
+
+## The maximum distance a thing can be targeted from.
+@export var detection_radius : float = 100.0 :
+	set(value):
+		$DetectionCircle/CollisionShape2D.shape.radius = value
+		detection_radius = value
+		return
+	get:
+		return detection_radius
+
+
+## Returns the [Area2D]s on the 3rd collision layer. 
+## Optionally you can pass in a [Callable] that somehow filters the Area2Ds 
+## if not provided, it will return all the Area2Ds
+## The filter functions should:
+## - take a single Variant argument
+## - return a boolean
+func get_targets(filter_func := Callable(_default_filter_func)):
+	if not $DetectionCircle.has_overlapping_areas():
+		return null
+	return $DetectionCircle.get_overlapping_areas().filter(filter_func)
+	
+## Instantiates the projectile(s) according to the information inside the [Projectile] resource.
+## Returns an [Array[Node]] of the instantiated projectiles.  [br]
+## Optionally you can pass in a [param map_func] [Callable] that modifies the instantiation process. [br]
+## The map functions should: [br]
+## - take a single Variant argument [br]
+## - return a Node
+func spawn_projectile(projectile : Projectile, map_func := Callable(_default_map_func)) -> Array[Node]:
+	var projectiles : Array[Node] = []
+
+	# Instatiate the projectiles
+	for i in range(projectile.projectiles_per_attack):
+		var new_projectile = projectile.scene.instantiate()
+		projectiles.append(new_projectile)
+
+	# Prepare the projectiles for spawn
+	projectiles = projectiles.map(map_func)
+
+	# Return
+	return projectiles
+
+## Returns a pseudo-random [Area2D] from the [param array] [b] or a [null] if none were found/passed the filter [/b].
+## The closer the target is, the higher the chance it will get targeted. [br]
+## Optionally you can pass in a [param filter_func] [Callable] that somehow filters the Area2Ds
+## if not provided, it will return all the Area2Ds
+## The filter functions should: [br]
+## - take a single Variant argument [br]
+## - return a boolean
+func distance_weighted_random_choice(array : Array[Area2D], max_distance : float, filter_func := Callable(_default_filter_func)):
+	# Filter the array
+	array = array.filter(filter_func)
+
+	# Idiot(me)-proofing
+	if len(array) == 0:
+		return null
+	
 	# Basically the closer the target is the higher the chance it will get targeted
 	if len(array) == 1:
 		return array[0]
