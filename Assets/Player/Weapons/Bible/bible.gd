7e25889cfaa1c09a6dfdd6de5ed011952e1da752
pkepowicz <paw.kepowicz@gmail.com>
2024-03-06 00:26:46 +0100

    placement logic

--- Assets/Player/Weapons/Bible/bible.gd
+++ Assets/Player/Weapons/Bible/bible.gd
@@ -1,7 +1,7 @@
 extends Weapon
 
 @onready var bible_projectile = preload("res://Assets/Player/Weapons/Bible/bible_projectile.tscn")
-@export var projectile_number = 5
+@export var projectile_number = 3
 @export var projectile_size = 1.0
 @export var damage = 1
 @export var radius = 2
@@ -16,9 +16,9 @@ func _ready():
 	
 func start_cycle():
 	instantiate_projectiles()
-	await get_tree().create_timer(5.0).timeout
+	await get_tree().create_timer(lifetime).timeout
 	delete_projectiles()
-	await get_tree().create_timer(2.0).timeout
+	await get_tree().create_timer(cooldown).timeout
 	start_cycle()
 	# instantiate projectile number of objects
 	# Wait for lifetime
@@ -26,9 +26,16 @@ func start_cycle():
 	# after cooldown repeat
 
 func instantiate_projectiles():
+	var player_position = Vector2(0, 0)
+	var angle_increment = 360.0 / projectile_number
+
 	print("Instantiate bible projectiles")
 	for i in range(projectile_number):
+		var angle = deg_to_rad(i * angle_increment)
+		var x = radius * cos(angle)
+		var y = radius * sin(angle)
 		var projectile_to_add = bible_projectile.instantiate()
+		projectile_to_add.position = Vector2(x,y)
 		add_child(projectile_to_add)
 		current_projectiles.append(projectile_to_add)
 
