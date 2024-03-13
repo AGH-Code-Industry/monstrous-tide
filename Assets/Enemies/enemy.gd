918b6878601096a39e4a491649bc5b6e808d3a87
pkepowicz <paw.kepowicz@gmail.com>
2024-03-13 17:07:07 +0100

    Integrated with preexisting changes

--- Assets/Enemies/enemy.gd
+++ Assets/Enemies/enemy.gd
@@ -4,22 +4,21 @@ extends CharacterBody2D
 @export var movement_speed = 20.0
 
 @onready var player = get_tree().get_first_node_in_group("player")
-@onready var sprite = $Polygons
-@onready var animator = $AnimationPlayer
+@onready var visuals = $Visuals
+var starting_scale : float
 
 func _ready():
-	pass
-	#animator.play("walk")
+	starting_scale = visuals.scale.x
 
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * movement_speed
 	move_and_slide()
 
-	#if direction.x > 0.1:
-		#sprite.flip_h = true
-	#elif direction.x < 0.1:
-		#sprite.flip_h = false
+	if direction.x > 0.1:
+		visuals.scale.x = -starting_scale
+	elif direction.x < 0.1:
+		visuals.scale.x = starting_scale
 		
 
 func _on_hit_box_on_death() -> void:
