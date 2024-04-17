7730da9b4224ee8bccbcd9176558c1799fddd676
pkepowicz <paw.kepowicz@gmail.com>
2024-04-17 18:09:18 +0200

    updated animation

--- Assets/Player/Weapons/Whip/whip_projectile.gd
+++ Assets/Player/Weapons/Whip/whip_projectile.gd
@@ -5,6 +5,7 @@ var line
 var head = Vector2(0,0)
 var target_position
 var player_ref
+var is_returning : bool = false
 
 func initialization(target, player):
 	target_position = target - position
@@ -16,11 +17,17 @@ func initialization(target, player):
 
 
 func _physics_process(delta):
-	head = head.lerp(target_position, 0.5)
-	line.set_point_position(0, player_ref.global_position - position)
-	line.set_point_position(1, head)
+	var player_position = player_ref.global_position - position
+	line.set_point_position(0, player_position)
+	if !is_returning:
+		head = head.lerp(target_position, 0.7)
+		line.set_point_position(1, head)
+	else:
+		head = head.lerp(player_position, 0.5)
+		line.set_point_position(1, head)
+		if head.is_equal_approx(Vector2.ZERO) : queue_free()
 	if head.is_equal_approx(target_position): 
-		queue_free()
+		is_returning = true
 	
 
 func lifetime():
