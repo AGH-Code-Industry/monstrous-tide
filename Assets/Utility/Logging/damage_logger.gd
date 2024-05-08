6ef059a851d61aac0cbe68ba0b1b1c94e17464a4
ddziechciarz <alawon12@gmail.com>
2024-05-08 21:18:56 +0200

    better logger and gitignore

--- Assets/Utility/Logging/damage_logger.gd
+++ Assets/Utility/Logging/damage_logger.gd
@@ -2,6 +2,7 @@ extends Node
 
 var damage_dict : Dictionary
 var time_dict: Dictionary
+var logger_array: Array
 
 func _ready() -> void:
 	clear_dicts()
@@ -9,15 +10,22 @@ func _ready() -> void:
 func clear_dicts() -> void:
 	damage_dict = {}
 	time_dict = {}
-
+	
 func log_damage(weapon_name: String, dmg_amount: float):
 	if ! damage_dict.has(weapon_name):
 		damage_dict[weapon_name] = 0
+		time_dict[weapon_name] = 0
 	damage_dict[weapon_name] += dmg_amount
 	print(damage_dict)
 
-func log_pickup_time(weapon_name: String):
-	pass #time_dict[weapon_name] = 
-
 func _process(delta: float) -> void:
-	pass #print(Time.get_ticks_usec())
+	# implement checking if player is on correct scene, eg. not shop etc.
+	for weapon in time_dict.keys():
+		time_dict[weapon] += delta
+
+func get_logged_values() -> Array:
+	var result = []
+	for weapon in damage_dict.keys():
+		result.append([weapon, damage_dict[weapon], time_dict[weapon]])
+	return result
+
